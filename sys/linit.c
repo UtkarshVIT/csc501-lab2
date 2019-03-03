b2e2a9117ee23b2ae76082484f94a05e5dfc8916
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-03-03 15:06:44 -0500

    sdf

--- sys/linit.c
+++ sys/linit.c
@@ -23,7 +23,7 @@ void linit()
 		lptr -> nreaders = 0;
 		lptr -> nwriters = 0;
 		lptr -> version = 0;
-		kprint("%d, %d", i, lock[i].version);
+		kprint("%d, %d", i, locks[i].version);
 	}
 
 	for(i = 0; i < NPROC; i ++)
