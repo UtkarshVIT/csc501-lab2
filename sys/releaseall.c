52eba93d3cc7d6b50d2ae7d44a9d8e30b54e2cbf
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-03-03 04:42:56 -0500

    tb

--- sys/releaseall.c
+++ sys/releaseall.c
@@ -53,8 +53,7 @@ int bestone(int lock)
         return highest;
     }   
 
-    int releaseall(int numlocks, int ldes1, ...)
-    {
+    int releaseall(int numlocks, int ldes1, ...){
     	int ldes, flag = 0;
     	unsigned long *a = (unsigned long *)(&ldes1);
     	for ( ; numlocks > 0 ; numlocks--)
@@ -147,3 +146,71 @@ int bestone(int lock)
     		if(flag == 1) return(SYSERR);
     		else return(OK);
     	}
+
+        int release(int pid, int ldes){
+            STATWORD ps;
+            register struct lentry  *lptr;
+            int highest, lockindex = ldes / 100;
+
+            disable(ps);
+            if (isbadlock(lockindex) || (lptr= &locks[lockindex])->lstate==LFREE || lptr -> version != ldes % 100) 
+            {
+                restore(ps);
+                return(SYSERR);
+            }
+
+            if(lockholdtab[pid][lockindex] > 0) lockholdtab[pid][lockindex]--;
+            else
+            {
+        //kprintf("released a lock that you don't hold!\n");
+                restore(ps);
+                return(SYSERR);
+            }
+
+            if((lptr -> nreaders > 0) && (lptr -> nwriters == 0)) lptr -> nreaders --;
+            else if((lptr -> nreaders == 0) && (lptr -> nwriters == 1)) lptr -> nwriters --;
+            else kprintf("Impossible to get here. Something is wrong!!!\n");
+
+            if((locks[lockindex].nreaders == 0) && (locks[lockindex].nwriters == 0))
+            {
+                highest = bestone(lockindex);
+                while(highest != -1)
+                {
+                    if(q[highest].qtype == READ)
+                    {
+                        lptr -> nreaders ++;
+                        lockholdtab[highest][lockindex] ++;
+                        dequeue(highest);
+                        ready(highest, RESCHNO);
+                        highest = bestone(lockindex);
+
+                        if((highest != -1) && (q[highest].qtype == WRITE))
+                        {
+                            int item = q[lptr -> lqtail].qprev;
+                    //while(item != lptr -> lqhead)
+                    //{
+                        //if((q[item].qkey >= q[highest].qkey) && (q[item].qtype == READ)) 
+                        //{
+                            //highest = item;
+                            //kprintf("HELLO BROTHER RAADHU SWAMI\n");
+                            break;
+                        //}
+                        //item = q[item].qprev;
+                    //}
+                            if(item == lptr -> lqhead) highest = -1;
+                        }
+                    }
+                    else if(q[highest].qtype == WRITE)
+                    {
+                        lptr -> nwriters ++;
+                        lockholdtab[highest][lockindex] ++;
+                        dequeue(highest);
+                        ready(highest, RESCHNO);
+                        break;
+                    }
+                }
+            }
+            restore(ps);
+            return OK;
+        }
+
