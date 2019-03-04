0f9c902d8e338104627e9bc2f3dd259c4fbd3325
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-03-04 14:25:07 -0500

    Update lock.h

--- sys/lock.h
+++ sys/lock.h
@@ -7,17 +7,19 @@
 
 
 struct lock_data{
-	int lock_type;		// check state of the lock
+	int lock_type;
+	int lock_qhead;  
+	int lock_lqtail;		// check state of the lock
 	int reader_count;
 	int writer_count;
-	int lock_qhead;  
-	int lock_lqtail;
+
 };
 
 extern struct lock_data lock_list[NLOCKS];
-
+/*
 int lock (int ldes1, int type, int priority);
 void linit();
 int ldelete(int lockdescriptor);
 int releaseall(int, int, ...);
 int lcreate();
+*/
