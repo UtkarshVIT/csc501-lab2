39d9edaba3546850a5998f6c837c7dbe06409f60
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-03-04 02:00:28 -0500

    Update releaseall.c

--- sys/releaseall.c
+++ sys/releaseall.c
@@ -116,10 +116,10 @@ int get_next_process(int lock_index, int *high_prio){
         return best_reader;
     }
     else{
-        if((best_reader_wait - best_writer_wait)>600){
+        /*if((best_reader_wait - best_writer_wait)>600){
             *high_prio=-1;
             return best_writer;
-        }
+        }*/
         *high_prio= best_reader_priority;
         return best_reader;
     }
