59edb1864105f7d9d716f2517c1b0ba69447d727
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-03-03 20:44:43 -0500

    Update lock.c

--- sys/lock.c
+++ sys/lock.c
@@ -13,46 +13,43 @@ void insert_in_prio_queue(int lock_index, int priority, int lock_type){
 	resched();
 }
 
-int lock(int lock_index, int lock_type, int priority) {
+int lock(int lock_index, int lock_type, int priority){
 	STATWORD ps;
 	disable(ps);
 
 	proctab[currpid].plwaitret = OK;
 
 	/* If the lock lock_type in LNONE */
-	if(ltable[lock_index].ltype == LNONE) {
+	if(ltable[lock_index].ltype == LNONE){
 		ltable[lock_index].ltype = lock_type;
 		proctab[currpid].locktype[lock_index] = lock_type;
 		ltable[lock_index].holders[currpid] = lock_type;
 
-		if(lock_type == READ) {
+		if(lock_type == READ){
 			ltable[lock_index].nreaders++;
 		}
 	}
 
 	/* If the lock lock_type for this already READ */
-	else if(ltable[lock_index].ltype == READ) {
+	else if(ltable[lock_index].ltype == READ){
 		
 		/* If the request lock_type is WRITE */
-		if (lock_type == WRITE) {
+		if (lock_type == WRITE)
 			insert_in_prio_queue(lock_index, priority, lock_type);
-			
-		}
 		
 		/* If the request lock_type is READ */
-		else if(lock_type == READ) {
-			int temp;
-			int flag = 0;
-			temp = q[ltable[lock_index].lqtail].qprev;
+		else if(lock_type == READ){
+			int ctr, flag = 0;
+			ctr = q[ltable[lock_index].lqtail].qprev;
 
-			for (temp = q[ltable[lock_index].lqtail].qprev; (temp != ltable[lock_index].lqhead) && (priority < q[temp].qkey); temp = q[temp].qprev) {
-				if (proctab[temp].locktype[lock_index] == WRITE) {
+			for (ctr = q[ltable[lock_index].lqtail].qprev; (ctr != ltable[lock_index].lqhead) && (priority < q[ctr].qkey); ctr = q[ctr].qprev) {
+				if (proctab[ctr].locktype[lock_index] == WRITE) {
 					flag = 1;
 					break;
 				}
 			}
 
-			if(flag) {
+			if(flag){
 				insert_in_prio_queue(lock_index, priority, lock_type);
 			}
 
@@ -65,7 +62,7 @@ int lock(int lock_index, int lock_type, int priority) {
 		}
 	}
 
-	else if(ltable[lock_index].ltype == WRITE) {
+	else if(ltable[lock_index].ltype == WRITE){
 		insert_in_prio_queue(lock_index, priority, lock_type);
 	} 
 
