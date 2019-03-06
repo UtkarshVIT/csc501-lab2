a33b9f674d75c383a219b4ef020b91324b774183
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-03-05 22:42:32 -0500

    asd

--- sys/main.c
+++ sys/main.c
@@ -184,97 +184,16 @@ void test3 ()
         kprintf ("Test 3 OK\n");
 }
 
-
-/*----------------------------------Test 4---------------------------*/
-void semaphoreProc4(char *msg, int sem){
-        kprintf ("  %s: to acquire sem\n", msg);
-        wait(sem);
-        kprintf ("  %s: acquired sem, sleep 1s\n", msg);
-        sleep(1);
-        kprintf ("  %s: to release sem\n", msg);
-        signal(sem);
-}
-void randomSemProc4(char *msg, int lck){
-        int i;
-        kprintf ("starting %s\n", msg);
-        for(i=0;i<100;i++)
-                kprintf("%s", msg);
-        kprintf ("\n %s sleeping for 3sec.\n");
-        
-        sleep(1);
-        
-        i=0;
-        for(i=0;i<100;i++)
-                kprintf("%s", msg);
-        kprintf("\n");
-    }
-void writer4(char *msg, int lck){
-        kprintf ("  %s: to acquire lock\n", msg);
-        lock(lck, WRITE, DEFAULT_LOCK_PRIO);
-        kprintf ("  %s: acquired lock, sleep 1s\n", msg);
-        sleep(1);
-        kprintf ("  %s: to release lock\n", msg);
-        releaseall(1, lck);
-}
-
-void random4(char *msg, int lck){
-        int i;
-        kprintf ("starting %s\n", msg);
-        for(i=0;i<100;i++)
-                kprintf("%s", msg);
-        kprintf ("\n %s sleeping for 3sec.\n");
-        
-        sleep(2);
-        
-        i=0;
-        for(i=0;i<100;i++)
-                kprintf("%s", msg);
-        kprintf("\n");
-    }
-
-void testSem(){
-    int semap = screate(1);
-    int sem1, sem2, sem3;
-    sem1 = create(semaphoreProc4, 2000, 25, "sem1", 2, "S1", semap);
-    sem2 = create(randomSemProc4, 2000, 30, "sem2", 2, "R1", semap);
-    sem3 = create(semaphoreProc4, 2000, 35, "sem3", 2, "S2", semap);
-
-    resume(sem1);
-    resume(sem2);
-    sleep(1);
-    resume(sem3);
-    
-}
-
-void testCustomLocks(){
-    int lock = lcreate();
-    int writer1, random1, writer2;
-
-    writer1 = create(writer4, 2000, 20, "writer1", 2, "W1", lock);
-    random1 = create(random4, 2000, 25, "random1", 1, "R1", lock);
-    writer2 = create(writer4, 2000, 30, "writer2", 2, "W2", lock);
-
-    resume(writer1);
-    resume(random1);
-    sleep(1);
-    resume(writer2);
-    sleep(10);
-}
 int main( )
 {
-	//test1();
-	//test2();
-	//stest3();
-
-    kprintf("\n-----------------using custom locks-------------------\n");
-    testCustomLocks();
-    kprintf("\n-----------------using Xinu sem-------------------\n");
-    testSem();
+	test1();
+	test2();
+	test3();
 
         /* The hook to shutdown QEMU for process-like execution of XINU.
          * This API call exists the QEMU process.
          */
-        //shutdown();
+    shutdown();
     return 0;
 }
 
