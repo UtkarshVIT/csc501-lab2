02be23c8869b28575132cb69de9d9815c7224139
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-04-25 19:50:06 -0400

    Update main.c

--- sys/main.c
+++ sys/main.c
@@ -705,8 +705,8 @@ int main( )
      //test3();
      //test4();
      //test5();
-     test6();
-     /*test7();*/
+     //test6();
+     test7();
      //test8();
      //test9();
      //test10();
