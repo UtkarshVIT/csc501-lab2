493fbb3d7a3c0023a05aa5943315517bdc1a9fc3
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-04-26 13:31:22 -0400

    Update lcreate.c

--- sys/lcreate.c
+++ sys/lcreate.c
@@ -8,7 +8,7 @@ int lcreate(){
     disable(ps);
     while(i < NLOCKS){
         if(lock_list[i].lock_type == DELETED || lock_list[i].lock_type == FREE){
-            lock_list[i].lock_type = FREE;
+            lock_list[i].lock_type = 22;
             kprintf("\nLock found. Returning lock %d", i);
             restore(ps);
             return i;
