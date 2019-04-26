26347ebe8f0d8d11871197a19bc9406d09bd605d
Utkarsh Sharma <utkarshsharma351@gmail.com>
2019-04-25 20:25:29 -0400

    Update main.c

--- sys/main.c
+++ sys/main.c
@@ -708,13 +708,13 @@ int main( )
      //test2();
      //test3();
      //test4();
-     test5();
+     //test5();
      //test6();
      //test7();
      //test8();
      //test9();
      //test10();
-     //test11();
+     test11();
      //test12();
     kprintf("\n--------------SUMMARY------------\n");
     kprintf("PASSED: %d, FAILED: %d, TOTAL: %d\n",totalpassed,totalfailed,totalpassed+totalfailed);
